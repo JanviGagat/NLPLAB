087d6ea5a6a4ed2b841162a9a43aa8007bc4019d
JanviGagat <janvigagat19@gmail.com>
2024-09-02 08:37:01 +0530

    Modified

--- test1.py
+++ test1.py
@@ -3,3 +3,23 @@ print("YO YO HONEY SINGH")
 print("PENCHO FRIDAY AA")
 print("YOYO HONEY SINGH")
 print("MOdified in GitHub")
+## for initializing the git hub
+# git init
+
+## for getting the status of the file
+#git status (it will occur in red color)
+
+## for loading the data
+# git add --all
+
+## for getting the status of the file
+#git status (it will occur in green color)
+
+## for commiting the file 
+# git commit -m "write any message if you want to"
+
+## for push file on github
+# git push
+
+## for pullig changes
+# git pull
